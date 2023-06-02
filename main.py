6534b0c6a29bdf530ad223acfda474d05b79d8d1
Vladimir <vovakarpov2017@gmail.com>
2023-06-02 21:53:03 +0400

    Created main.py and implemented searching for card numbers with a given hash

---
+++ main.py
@@ -0,0 +1,28 @@
+import logging
+import argparse
+import json
+
+from functions import searching
+
+
+logger = logging.getLogger()
+logger.setLevel('INFO')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Hash Function Collision Search')
+    parser.add_argument('mode', type=str, help='1 - Search for card numbers with a given hash')
+    parser.add_argument('--config', type=str, help='Name of config file', default="Data/data.json")
+    args = parser.parse_args()
+    data_path = args.config
+
+    try:
+        with open(args.config, 'r') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        logging.error(f"{args.config} not found")
+
+    if args.mode == "1":
+        logging.info('Search for the card number...\n')
+        searching(data, int(data["processes_amount"]))
+        logging.info('Card number search completed')
