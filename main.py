0c4e541ac66f4f11d4f5e799815f961a987dc326
Vladimir <vovakarpov2017@gmail.com>
2023-06-02 22:16:53 +0400

    Added output of the dependence of the execution time on the number of threads

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import logging
 import argparse
 import json
-
-from functions import searching, luhn
+from functions import searching, luhn, get_stats
 
 
 logger = logging.getLogger()
@@ -12,7 +11,9 @@ logger.setLevel('INFO')
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Hash Function Collision Search')
     parser.add_argument('mode', type=str, help='1 - Search for card numbers with a given hash '
-                                               '2 - Check the card for authenticity ')
+                                               '2 - Checks the card for authenticity '
+                                               '3 - Output of the dependence of the execution time on the number of '
+                                               'threads')
     parser.add_argument('--config', type=str, help='Name of config file', default="Data/data.json")
     args = parser.parse_args()
     data_path = args.config
@@ -31,5 +32,9 @@ if __name__ == '__main__':
         logging.info('Checking the correctness of the card...')
         luhn(data)
         logging.info('Verification of the cards correctness is completed')
+    elif args.mode == "3":
+        logging.info('Data collection...\n')
+        get_stats(data)
+        logging.info('Data collection completed successfully \n')
     else:
         logging.error("something went wrong... Try again")
